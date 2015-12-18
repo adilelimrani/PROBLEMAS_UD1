18af72c82f4fd30838e530f4dbb5648f58cd3314
adilelimrani <adilelimrani@gmail.com>
2015-12-18 13:51:52 +0100

    Create 1.3.cpp

---
+++ 1.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double x=5,y=2;
+
+	cout << "valor de x: "<< x << endl;
+	cout << "valor de y; " << y << endl;
+	cout << "valor de x/y\n"<<x/y;
+	cout << "\tvalor de y/x"<<y/x<<endl;
+	cout << "la mitad de x: "<<x/2.0<<endl;
+	return 0;
+}
